fabaa8ab28668c19cc2edf9dfbe14ad57564451c
unknown <herry220388@gmail.com>
2019-03-09 15:31:38 +0700

    like and dislike

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,7 @@ Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@like');
+
+Route::delete('/like/{reply}', 'LikeController@dislike');
